d17f367ffa7d1b3d687d22837bb779de9370aed1
ElOtroNach0 <javierignacio.peralta@gmail.com>
2022-12-13 19:19:06 -0300

    segundo entrega

--- main.js
+++ main.js
@@ -1,61 +1,89 @@
+// Objeto construstor.
+
+class Vuelo {
+    constructor(destino, precio){
+        this.destino = destino;
+        this.precio = precio;
+    }
+}
+
+const oferta1 = new Vuelo("Bariloche", 15000);
+const oferta2 = new Vuelo("Salta", 11000);
+const oferta3 = new Vuelo("Mendoza", 25000);
+
+const arrayVuelos = [];
+
+arrayVuelos.push(oferta1);
+arrayVuelos.push(oferta2);
+arrayVuelos.push(oferta3);
+
+console.log(arrayVuelos);
+
+// FUNCIONES 
+
+// Fn Bienvenida
 const saludar = () => {
     alert ('Bienvenido a Pasaporte!\n Primer sitio de subastas de vuelos');
 }
 
-const ofertas = () => {
-    let oferta = Number(prompt('Para que vuelo queres ofertar? \n1-Bariloche\n2-Mendoza\n3-Salta'));
+// Fn Seleccion de destinos que se pueden ofertar.
+const destinos = () => {
+    let oferta = Number(prompt('Para que vuelo queres ofertar? \n1-Bariloche\n2-Mendoza\n3-Salta\n4-Salir'));
 
-    while(oferta >= 4 || oferta == 0 || isNaN(oferta)){
-        console.log(oferta);
-        
-        oferta = Number(prompt('No es posible ofertar!, Los vuelos para ofertar son: \n1-Bariloche\n2-Mendoza\n3-Salta'));
+    while(oferta >= 5 || oferta == 0 || isNaN(oferta)){
+        oferta = Number(prompt('No es posible ofertar!, Los vuelos para ofertar son: \n1-Bariloche\n2-Mendoza\n3-Salta\n4-Salir'));
     }
 
-    switch(oferta){
+    switch(oferta) {
         case 1:
-            return ultimoMontoA = 35000;  
+            return "Bariloche";
         case 2:
-            return ultimoMontoB = 11000;
+            return "Mendoza";
         case 3:
-            return ultimoMontoC = 10000;
+            return "Salta";
+        case 4:
+            return salir();
+            break;
     }
 
 }
+// Fn Ingrese de monto a ofertar para el destino seleccionado.
+const ofertar = (a) =>{
+    const destino = a;
 
-const destinos = (a) =>{
-    let ultimoMontoA = 0;
-    let ultimoMontoB = 0;
-    let ultimoMontoC = 0;
+    const precio = Number(prompt("Ingresar el monto a ofertar, tene en cuenta que tiene que ser mayor al monto anterior"));
+    const oferta = new Vuelo(destino, precio);
+    arrayVuelos.push(oferta);
 
-    if (ultimoMontoA === 3500){
-        return destino = "Bariloche";
-    }else if(ultimoMontoB === 11000){
-        return destino ="Mendoza";
-    }else if(ultimoMontoC === 10000){
-        return destino = "Salta";
+    return precio;
+}
+
+// Fn comprueba si el valor ingresado es menor al ultimo ofertado.
+
+const compararSubasta = (precio) => {
+    if (arrayVuelos.find(precios => precios.precio < precio)){
+        alert ("Tu oferta fue ingresada correctamente");
+        salir()
+    }else {
+        alert("Tu monto es menor a la ultima ofertada, por favor oferta un poco mas");
+            const SalirOSeguir = prompt("Queres volver a probar suerte? \n\nSi/No");
+
+        if (SalirOSeguir == "si"){
+            destinos();
+        }else{
+            salir();
+        }
+
     }
 }
 
-const subasta = (a, b) =>{
-        do{
-            respuestaOferta = Number(prompt('Estas por ofertar por un vuelo a ' + b + ', \n\n Cuanto queres ofertar? \n\nRecorda que tenes 3 posibilidades, se va a tomar la que supere el ultimo monto ofertado'));
-        }while(isNaN(respuestaOferta) || respuestaOferta == '')
-       
-        if (a){
-            for(i=2; i > 0; i--){
-                if(respuestaOferta >= oferta){
-                    alert ('Muy bien!! Tu oferta fue de $' + respuestaOferta + ' dentro de las proximas horas vas a saber si el ticket es tuyo');
-                    break;
-                        
-                }else{
-                    respuestaOferta = Number(prompt('Tu oferta de $' + respuestaOferta + ' esta por debajo! Te quedan ' + i + ' posibilidades'));
-                }
-            }
-}
-}
+//  Fn Salir del programa
 
+const salir = () => {
+    alert ("Nos vemos la proxima!");
+}
 
 saludar();
-let oferta = ofertas();
-let destino = destinos(oferta);
-let resultado = subasta(oferta, destino);
+let fnDestinos = destinos();
+let fnOfertar = ofertar(fnDestinos);
+let fnPrecio = compararSubasta(fnOfertar);
