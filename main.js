50113a3e60eac2dda10825ae6d60a2d75dd6295d
ElOtroNach0 <javierignacio.peralta@gmail.com>
2022-12-29 23:25:12 -0300

    entrega

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 // Objeto construstor.
 
 class Vuelo {
-    constructor(destino, precio){
+    constructor(id, destino, precio, img){
+        this.id = id;
         this.destino = destino;
         this.precio = precio;
+        this.img = img;
     }
 }
 
-const oferta1 = new Vuelo("Bariloche", 15000);
-const oferta2 = new Vuelo("Salta", 11000);
-const oferta3 = new Vuelo("Mendoza", 25000);
+const oferta1 = new Vuelo(1, "Bariloche", 15000, "./img/bariloche.jpg");
+const oferta2 = new Vuelo(2, "Salta", 11000, "./img/salta.jpg");
+const oferta3 = new Vuelo(3, "Mendoza", 25000, "./img/mendoza.jpg");
 
 const arrayVuelos = [];
 
@@ -19,81 +21,51 @@ arrayVuelos.push(oferta3);
 
 console.log(arrayVuelos);
 
+let arrayOfertas = [];
+
+// conexion al html
+const mainContainer = document.getElementById("main-container");
+const mainContainerFormulario = document.getElementById("main-container-formulario");
+
 // FUNCIONES 
+// Funcion para mostrar los vuelos a los cuales se puede ofertar.
 
-// Fn Bienvenida
-const saludar = () => {
-    alert ('Bienvenido a Pasaporte!\n Primer sitio de subastas de vuelos');
-}
+const mostrarVuelos = () => {
+    arrayVuelos.forEach(vuelo => {
+       const cardVuelos = document.createElement("div");
+       cardVuelos.classList.add("col-xl-3", "col-md-6", "col-xs-12"); 
+       cardVuelos.innerHTML = `
+        <div class="card">
+            <img src="${vuelo.img}" class="card-img-top" alt="${vuelo.destino}">
+            <div class="card-body">
+                <h3>${vuelo.destino}</h3>
+                <p>Ultimo precio:$ ${vuelo.precio}</p>
+                <button type="button" class="btn btn-success" id="boton${vuelo.id}">Ofertar</button>
+            </div>
+        </div>
+       `
+       mainContainer.appendChild(cardVuelos);
 
-// Fn Seleccion de destinos que se pueden ofertar.
-const destinos = () => {
-    let oferta = Number(prompt('Para que vuelo queres ofertar? \n1-Bariloche\n2-Mendoza\n3-Salta\n4-Salir'));
+        const boton = document.getElementById(`boton${vuelo.id}`);
+        const botonActivado = document.getElementById("botonActivado");
 
-    while(oferta >= 5 || oferta == 0 || isNaN(oferta)){
-        oferta = Number(prompt('No es posible ofertar!, Los vuelos para ofertar son: \n1-Bariloche\n2-Mendoza\n3-Salta\n4-Salir'));
-    }
+        boton.addEventListener("click", ()=>{
+            botonActivado.classList.toggle("active");
 
-    if(oferta < 4 ){
-        return oferta;
+        })
+
+
+        })
+    };
+
+mostrarVuelos();
+
+const ultimaOferta = (value) => {
+    const ofertaPrecio = arrayVuelos.find(vuelo => vuelo.precio === value);
+    if(ofertaPrecio && ofertaPrecio > precio){
+        ofertaPrecio.precio
+        console.log("Tu precio se ingreso correctamente");
     }else{
-        salir();
-    }
-
-}
-// Fn Ingrese de monto a ofertar para el destino seleccionado.
-const ofertar = (a) =>{
-    const destino = a;
-   
-    const precio = Number(prompt("Ingresar el monto a ofertar, tene en cuenta que tiene que ser mayor al monto anterior"));
-
-    const oferta = new Vuelo(destino, precio);
-    arrayVuelos.push(oferta);
-
-    return precio;
-}
-
-// Fn comprueba si el valor ingresado es menor al ultimo ofertado.
-
-const compararSubasta = (precio) => {
-    if (arrayVuelos.find(precios => precios.precio < precio)){
-        alert ("Tu oferta fue ingresada correctamente");
-        salir()
-    }else {
-        alert("Tu monto es menor a la ultima ofertada, por favor oferta un poco mas");
-        let aceptarCancelar = confirm("Queres volver a probar suerte?");
-
-        if (aceptarCancelar){
-            destinos();
-        }else{
-            salir();
-        };
-    }
-};
-
-//  Fn Salir del programa
-
-const salir = () => {
-    alert ("Nos vemos la proxima!");
-}
-
-
-// fn swich
-const seleccionCaso = (oferta) => {
-    switch(oferta) {
-        case 1:
-            return "Bariloche";
-        case 2:
-            return "Mendoza";
-        case 3:
-            return "Salta";
-        case 4:
-            return salir();
+        console.log("El precio ofertado es menor! Volve a intentarlo")
     }
 }
-
-saludar();
-let fnDestinos = destinos();
-let fnCasos = seleccionCaso(fnDestinos);
-let fnOfertar = ofertar(fnDestinos);
-let fnPrecio = compararSubasta(fnOfertar);
